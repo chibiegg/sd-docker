0093f2e48847072d98f5dea4196c1bcd1b2d85c8
chibiegg <chibiegg@chibiegg.net>
2024-06-28 13:32:22 +0900

    関数名変更

--- entrypoint_sd15.sh
+++ entrypoint_sd15.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 
-
-nvidia-smi
+if [ -z "${MODEL_FILE}" ]; then
+	MODEL_FILE=cityedgemix_v125.safetensors
+fi
+MODEL_FILE="/sd-models/${MODEL_FILE}"
 
 if [ -z "${SAKURA_ARTIFACT_DIR}" ]; then
 	SAKURA_ARTIFACT_DIR=/tmp
@@ -13,27 +15,32 @@ function gen_image () {
 	if [ -n "$2" ]; then
 		NUM_IMAGES="$2"
 	fi
+	if [ -z "${SAMPLER}" ]; then
+		SAMPLER="dpmsolver++"
+	fi
+	if [ -z "${STEPS}" ]; then
+		STEPS=50
+	fi
 	EXTRA_ARGS=""
 	if [ -n "${LORA_URL}" ]; then
 		LORA_FILENAME=`basename "${LORA_URL}"`
 		curl -o "/opt/${LORA_FILENAME}" "${LORA_URL}"
 		EXTRA_ARGS="--network_module networks.lora --network_weights /opt/${LORA_FILENAME}"
 	fi
-	python3 sdxl_gen_img.py \
-		--ckpt /sd-models/sd_xl_base_1.0.safetensors \
-		--vae /sd-models/sdxl_vae.safetensors \
+	if [ -n "${SEED}" ]; then
+		EXTRA_ARGS=" --seed ${SEED}"
+	fi
+	python3 gen_img.py \
+		--ckpt ${MODEL_FILE} \
 		--images_per_prompt ${NUM_IMAGES} ${EXTRA_ARGS} \
+		--sampler ${SAMPLER} --steps ${STEPS} \
 		--outdir ${SAKURA_ARTIFACT_DIR} --xformers --fp16 --prompt "${PROMPT}"
 }
 
-function study () {
+function gen_lora () {
 	DATA_URL="$1"
 	OUTPUT_NAME="$2"
 	CLASS_TOKENS="$3"
-
-	if [ -z "${NUM_PROCESS}" ]; then
-		NUM_PROCESS=2
-	fi
 
 	mkdir -p /opt/data
 	curl ${DATA_URL} | tar zxvf - -C /opt/data
@@ -41,20 +48,24 @@ function study () {
 	eval "echo \"$(cat /opt/lora_config.toml.tmpl)\"" > /opt/lora_config.toml	
 
 
-	accelerate launch --num_cpu_threads_per_process ${NUM_PROCESS} sdxl_train_network.py \
-		--pretrained_model_name_or_path='/sd-models/sd_xl_base_1.0.safetensors' \
+	accelerate launch --num_cpu_threads_per_process 1 train_network.py \
+		--pretrained_model_name_or_path="${MODEL_FILE}" \
 		--dataset_config='/opt/lora_config.toml' \
 		--output_dir="${SAKURA_ARTIFACT_DIR}" \
 		--output_name="${OUTPUT_NAME}" \
-		--save_model_as=safetensors --prior_loss_weight=1.0 --resolution=1024,1024 \
-		--train_batch_size=1 --max_train_epochs=15 --learning_rate=1e-4 \
+		 --resolution=512,512 --train_batch_size=1 --max_train_epochs=10 \
+		--save_model_as=safetensors --prior_loss_weight=1.0 \
+		--learning_rate=1e-4 --optimizer_type="AdamW8bit" \
 		--xformers --mixed_precision="fp16" --cache_latents \
-		--gradient_checkpointing --network_module=networks.lora --no_half_vae
+		--gradient_checkpointing --network_module=networks.lora
 }
+
+nvidia-smi
 
 TASK=$1
 
 case "${TASK}" in
 	"generate" ) gen_image "$2" "$3" ;;
-	"study" ) study "$2" "$3" "$4" ;;
+	"lerning" ) gen_lora "$2" "$3" "$4" ;;
+	"study" ) gen_lora "$2" "$3" "$4" ;;
 esac
